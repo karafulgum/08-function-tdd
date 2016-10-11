040fff36ce5e7101518933d0fd930e6f2a006b49
Kara Fulgum <karafulgum@gmail.com>
2016-10-11 10:53:21 -0500

    finished 04-selection-action-test

--- app/blackjack/select-action.js
+++ app/blackjack/select-action.js
@@ -2,5 +2,13 @@ import shouldSplit from './should-split';
 import shouldHit from './should-hit';
 
 export default function (card1, card2) {
-
+  if (shouldHit(card1, card2) === true) {
+    return 'hit';
+  }
+  if (shouldSplit(card1, card2) === true) {
+    return 'split';
+  }
+  if (shouldHit(card1, card2) === false) {
+    return 'stay';
+  }
 }
