af364ec56d993f89edf2e018c6c7421ed6cf81ec
Kara Fulgum <karafulgum@gmail.com>
2016-10-11 10:58:12 -0500

    Changed 04-select-action to if/else if/else statement

--- app/blackjack/select-action.js
+++ app/blackjack/select-action.js
@@ -4,11 +4,9 @@ import shouldHit from './should-hit';
 export default function (card1, card2) {
   if (shouldHit(card1, card2) === true) {
     return 'hit';
-  }
-  if (shouldSplit(card1, card2) === true) {
+  } else if (shouldSplit(card1, card2) === true) {
     return 'split';
-  }
-  if (shouldHit(card1, card2) === false) {
+  }  else {
     return 'stay';
   }
 }
